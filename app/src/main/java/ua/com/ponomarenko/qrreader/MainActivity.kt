1339e9495c3896f609bd6f15fe2499922de34c8a
Oleh Ponomarenko <albka1986@gmail.com>
2018-04-27 14:07:04 +0200

    provided requiring permission

--- app/src/main/java/ua/com/ponomarenko/qrreader/MainActivity.kt
+++ app/src/main/java/ua/com/ponomarenko/qrreader/MainActivity.kt
@@ -1,13 +1,62 @@
 package ua.com.ponomarenko.qrreader
 
-import android.support.v7.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.google.android.gms.vision.Frame
+import com.google.android.gms.vision.barcode.Barcode
+import com.google.android.gms.vision.barcode.BarcodeDetector
+
+private const val PERMISSION_CODE = 101
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        checkPermissions()
+
+        val myBitmap = BitmapFactory.decodeResource(applicationContext.resources, R.drawable.puppy)
+
+        val detector = BarcodeDetector.Builder(applicationContext).setBarcodeFormats(Barcode.DATA_MATRIX or Barcode.QR_CODE).build()
+
+        if (!detector.isOperational) {
+        }
+
+        val frame = Frame.Builder().setBitmap(myBitmap).build()
+        val barcodes = detector.detect(frame)
+
+        val thisCode = barcodes.valueAt(0)
+    }
+
+    private fun checkPermissions() {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), PERMISSION_CODE)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSION_CODE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                } else {
+                    finish()
+                }
+                return
+            }
+        }
     }
 }
