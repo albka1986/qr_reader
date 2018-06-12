d0409df35a7e6fa7b6e7d5066b44383f0522ca42
Oleh Ponomarenko <albka1986@gmail.com>
2018-06-12 18:28:57 +0300

    removed unnecessary class

--- app/src/main/java/com/ponomarenko/qrreader/details/fragments/BarcodeType.kt
+++
@@ -1,8 +0,0 @@
-package com.ponomarenko.qrreader.details.fragments
-
-/**
- * Created by Ponomarenko Oleh on 31.05.2018.
- */
-enum class BarcodeType {
-    GENERAL, CONTACT, WIFI, URL, EMAIL, PHONE, CALENDAR_EVENT, GEO_POINT, DRIVER_LICENSE
-}
